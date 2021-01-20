bc7528c4bf214f83149831c65bcec28a9e7cdaf0
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2021-01-20 16:57:47 +0300

    isLoggedIn работает в тестах

--- src/components/Home.spec.jsx
+++ src/components/Home.spec.jsx
@@ -5,13 +5,28 @@ import { render } from '@testing-library/react'
 
 describe("Home", () => {
   it("renders correctly", () => {
-    const { getByLabelText } = render(<HomeWithAuth />);
+    const { getByLabelText } = render(
+    <HomeWithAuth
+    isLoggedIn={false} 
+    navigate={() => undefined} 
+    logOut={() => undefined}
+    logIn={() => undefined}
+    />
+    );
     expect(getByLabelText('Имя пользователя:')).toHaveAttribute('name', 'email')
     expect(getByLabelText('Пароль*')).toHaveAttribute('name', 'password')
   });
   describe("when logged in", () => {
     it("renders profile link", () => {
-      const { getByText } = render(<HomeWithAuth isLoggedIn />);
+      const { getByText } = render(
+      <HomeWithAuth 
+      isLoggedIn={true}  
+      navigate={() => undefined} 
+      logOut={() => undefined}
+      logIn={() => undefined}
+      />
+      
+      );
       expect(getByText("go to profile")).toBeInTheDocument()
     });
   });
